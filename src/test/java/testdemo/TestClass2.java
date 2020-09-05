59edb97f4668ab1aee7db9ceafcda14acf0a97d9
Sonal <mittal.sonal04@gmail.com>
2020-09-04 20:28:31 -0400

    Signed-off-by: Sonal <mittal.sonal04@gmail.com>

---
+++ src/test/java/testdemo/TestClass2.java
@@ -0,0 +1,16 @@
+package testdemo;
+
+import org.testng.annotations.Test;
+
+public class TestClass2 {
+	
+
+	@Test
+	public void logout()
+	{
+		System.out.println("loginto functionality");
+	}
+
+}
+
+
