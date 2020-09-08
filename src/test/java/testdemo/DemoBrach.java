a1ce7170cafcf2c4d5de78fdcd851ba1deefa67f
Sonal <mittal.sonal04@gmail.com>
2020-09-07 20:53:00 -0400

    Signed-off-by: Sonal <mittal.sonal04@gmail.com>

---
+++ src/test/java/testdemo/DemoBrach.java
@@ -0,0 +1,15 @@
+package testdemo;
+
+import org.testng.annotations.Test;
+
+public class DemoBrach {
+	
+	@Test
+	public void Demoloan()
+	{
+		System.out.println("homeloan functionality");
+		System.out.println("extra functionality");
+		System.out.println("logout functionality");
+	}
+
+}
