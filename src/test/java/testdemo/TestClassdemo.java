0bfc4541d63ff34d95e802328d6f5bed62a43530
Sonal <mittal.sonal04@gmail.com>
2020-09-08 18:57:06 -0400

    Signed-off-by: Sonal <mittal.sonal04@gmail.com>

---
+++ src/test/java/testdemo/TestClassdemo.java
@@ -0,0 +1,14 @@
+package testdemo;
+
+public class TestClassdemo {
+	
+	public void newMethod()
+	{
+	
+	System.out.println("added a new method");
+	System.out.println("added a new method1");
+	System.out.println("added a new method2");
+		
+	}
+
+}
