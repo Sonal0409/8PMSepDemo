28d5f77f4d3625bc7a8971d62cf4f99c846689cb
Sonal Mittal <62133890+Sonal0409@users.noreply.github.com>
2020-09-08 20:22:19 -0400

    Update SonalDemo1.java

--- src/test/java/testdemo/SonalDemo1.java
+++ src/test/java/testdemo/SonalDemo1.java
@@ -7,6 +7,7 @@ public class SonalDemo1 {
 		System.out.println("hello worls");
 		System.out.println("hello Sonal");
 		System.out.println("hello Class");
+		System.out.println("heelo from priya");  // changes added by priya
 	}
 
 }
