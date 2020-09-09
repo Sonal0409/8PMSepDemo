4eded430a9f14289f6f9225834185c273ad1294f
Sonal <mittal.sonal04@gmail.com>
2020-09-08 20:13:05 -0400

    Signed-off-by: Sonal <mittal.sonal04@gmail.com> created a new Demo file

---
+++ src/test/java/testdemo/SonalDemo1.java
@@ -0,0 +1,12 @@
+package testdemo;
+
+public class SonalDemo1 {
+	
+	public void mentod1()
+	{
+		System.out.println("hello worls");
+		System.out.println("hello Sonal");
+		System.out.println("hello Class");
+	}
+
+}
